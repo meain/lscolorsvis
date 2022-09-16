225561ac4cdf43d7089774019a2fa243029a21e3
Abin Simon <mail@meain.io>
2022-09-16 14:00:17 +0530

    Optionally read from cli arg

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,28 @@ use lscolors::{Indicator, LsColors, Style};
 use std::env;
 
 fn main() {
-    // TODO: get input from cli args
-    let lscolors_env = env::var("LS_COLORS");
+    let args: Vec<String> = env::args().collect();
+    let lscolors_env = if args.len() > 1 {
+        Ok(args[1].clone())
+    } else {
+        env::var("LS_COLORS")
+    };
     if let Ok(le) = lscolors_env {
         let pieces: Vec<_> = le.split(":").collect();
         let lscolors = LsColors::from_string(&le);
 
-        for p in pieces {
-            if p.starts_with("*") {
-                let path = "file".to_string()
-                    + &p[1..].to_string().split("=").collect::<Vec<_>>()[0];
-                let style = lscolors.style_for_path(&path);
-
-                let ansi_style = style.map(Style::to_ansi_term_style).unwrap_or_default();
-                print!("{}  ", ansi_style.paint(path));
+        for entry in pieces {
+            let name = entry.split("=").collect::<Vec<_>>()[0];
+            let style = if entry.starts_with("*") {
+                Some(lscolors.style_for_path(&name))
             } else {
-                let indicator_str = p.split("=").collect::<Vec<_>>()[0];
-                let indicator = Indicator::from(indicator_str);
-                if let Some(ind) = indicator {
-                    let path = indicator_str;
-                    let style = lscolors.style_for_indicator(ind);
-
-                    let ansi_style = style.map(Style::to_ansi_term_style).unwrap_or_default();
-                    print!("{}  ", ansi_style.paint(path));
-                }
+                Indicator::from(name).and_then(|x| Some(lscolors.style_for_indicator(x)))
+            };
+            if let Some(s) = style {
+                let ansi_style = s.map(Style::to_ansi_term_style).unwrap_or_default();
+                print!("{} ", ansi_style.paint(entry));
             }
         }
     }
+    println!(""); // final newline
 }
